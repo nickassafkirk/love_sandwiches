f442830e2ffbbb66bf066711a912f575a3bb650b
nickassafkirk <nickassafkirk@gmail.com>
2021-05-05 21:15:54 +0000

    Calculate stock averages and update stock worksheet

--- run.py
+++ run.py
@@ -100,6 +100,24 @@ def get_last_5_entries_sale():
         
     return columns
 
+def calculate_stock_data(data):
+    """
+    Calculate the average stock for each day, adding 10%
+    """
+    print("Calculating stock data...\n")
+
+    new_stock_data = []
+
+    for column in data:
+        int_column = [int(num) for num in column]
+        average = sum(int_column) / len(int_column)
+        stock_num = average * 1.1
+        new_stock_data.append(round(stock_num))
+
+    return new_stock_data
+    
+
+
 
 def main():
     """
@@ -110,7 +128,9 @@ def main():
     update_worksheet(sales_data, 'sales')
     surplus = calculate_surplus_data(sales_data)
     update_worksheet(surplus, 'surplus')
+    sales_columns = get_last_5_entries_sale()
+    stock_data = calculate_stock_data(sales_columns)
+    update_worksheet(stock_data, "stock")
 
 print("Welcome to Love Sandwiches data automation")
-# main()
-get_last_5_entries_sale()
+main()
